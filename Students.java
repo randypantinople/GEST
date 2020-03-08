aaf07320e64f44a642d692cba51e6e1f9b9b7585
Randy Pantinople <randypantinople@yahoo.com>
2020-03-08 19:54:06 -0400

    Students class for the table students in the database

---
+++ Students.java
@@ -0,0 +1,30 @@
+public class Students {
+
+    private int studentId;
+    private String firstName;
+    private String lastName;
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
