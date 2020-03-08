dce256519e93dc5af78634bfebe1b856b2e5f852
Randy Pantinople <randypantinople@yahoo.com>
2020-03-08 19:52:25 -0400

    A class for querrying scores for each student

---
+++ ScoresForEachStudents.java
@@ -0,0 +1,30 @@
+public class ScoresForEachStudents {
+
+    private String firstName;
+    private String lastName;
+    private double score;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+}
