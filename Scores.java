c7cd2290923007c1866671c0d9e7b33f10e1dd12
Randy Pantinople <randypantinople@yahoo.com>
2020-03-08 19:50:36 -0400

    Scores class for the table scores in the database

---
+++ Scores.java
@@ -0,0 +1,21 @@
+public class Scores {
+    private int studentId;
+    private double scores;
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public double getScores() {
+        return scores;
+    }
+
+    public void setScores(double scores) {
+        this.scores = scores;
+    }
+}
+
